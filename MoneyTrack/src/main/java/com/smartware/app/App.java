8d02db2f06c4d145a4337675c7a472dac7758d72
Impreza Dev <imprezadev@gmail.com>
2017-03-05 20:50:50 -0500

    using GregorianCalendar type to set date as another date different than now

--- MoneyTrack/src/main/java/com/smartware/app/App.java
+++ MoneyTrack/src/main/java/com/smartware/app/App.java
@@ -2,12 +2,14 @@ package com.smartware.app;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 import com.smartware.domain.Transaction;
 
 public class App {
 
   public static void main(String[] args) {
-    Calendar cal = Calendar.getInstance();
+    Calendar cal = new GregorianCalendar(2017, Calendar.MARCH, 4);
     Date now = cal.getTime();
 
     Transaction trans = new Transaction();
