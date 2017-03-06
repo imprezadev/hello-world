a56fd095ccfb200768bbf652d074a4de3665428f
Impreza Dev <imprezadev@gmail.com>
2017-03-05 20:32:11 -0500

    using Calendar java type to set date as now

--- MoneyTrack/src/main/java/com/smartware/app/App.java
+++ MoneyTrack/src/main/java/com/smartware/app/App.java
@@ -1,12 +1,18 @@
 package com.smartware.app;
 
+import java.util.Calendar;
+import java.util.Date;
 import com.smartware.domain.Transaction;
 
 public class App {
- 
+
   public static void main(String[] args) {
+    Calendar cal = Calendar.getInstance();
+    Date now = cal.getTime();
+
     Transaction trans = new Transaction();
     trans.setId(1L);
+    trans.setDate(now);
     trans.setAmount(22f);
     trans.setConcept("Lavada del Impreza");
 
