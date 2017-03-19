f9779a0cbd6a7f69b104019a0ae31edc51b7f51f
Impreza Dev <imprezadev@gmail.com>
2017-03-19 09:13:54 -0500

    test code to show date in a desired format, using SimpleDateFormt class

--- MoneyTrack/src/main/java/com/smartware/app/App.java
+++ MoneyTrack/src/main/java/com/smartware/app/App.java
@@ -1,5 +1,8 @@
 package com.smartware.app;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.smartware.domain.Transaction;
 import com.smartware.service.TransactionService;
 
@@ -9,6 +12,11 @@ public class App {
 		TransactionService transactionService = new TransactionService();
 
 		System.out.println("Transaction: " + transactionService.getTransaction(3L));
+
+		// just to show the date in a desired format
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = transactionService.getTransaction(3L).getDate();
+		System.out.println("Transaction date: " + dateFormat.format(date));
 		
 		for (Transaction trans: transactionService.getTransactions()) {
 			System.out.println("Transaction: " + trans);
