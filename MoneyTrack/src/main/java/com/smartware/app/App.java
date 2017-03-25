52fa1c4f87070f5636be0b96d4083c3b86672f51
Impreza Dev <imprezadev@gmail.com>
2017-03-25 17:29:16 -0500

    REFACTOR: clean runnable main method, just leave here what I want to test

--- MoneyTrack/src/main/java/com/smartware/app/App.java
+++ MoneyTrack/src/main/java/com/smartware/app/App.java
@@ -1,55 +1,13 @@
 package com.smartware.app;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Logger;
-
-import com.smartware.domain.Expense;
-import com.smartware.domain.Transaction;
-import com.smartware.domain.catalog.Currency;
-import com.smartware.service.ExpenseService;
-import com.smartware.service.TransactionService;
 
 public class App {
 	
 	final static Logger logger = Logger.getLogger(App.class.getName());
 
 	public static void main(String[] args) {
-		TransactionService transactionService = new TransactionService();
-		ExpenseService expenseService = new ExpenseService();
-
-		logger.info("Transaction: " + transactionService.getTransaction(3L));
-
-		// just to show the date in a desired format
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		Date date = transactionService.getTransaction(5L).getDate();
-		logger.info("Transaction date: " + dateFormat.format(date));
-
-		String transactionList = "";
-		for (Transaction trans: transactionService.getTransactions()) {
-			transactionList += "\n\t" + trans;
-		}
-		logger.info("Transaction:" + transactionList);
-		
-		for (Currency cur: Currency.values()) {
-			logger.info(cur.name());
-		}
-
-		Transaction transaction = new Transaction();
-		transaction.setDate(new Date());
-		transaction.setAmount(23.70f);
-		transaction.setCurrency(Currency.PEN);
-		transaction.setConcept("Almuerzo Liverpool");
-
-		logger.info(transaction.toString());
-
-		transactionService.insertTransaction(transaction);
-
-		String expenseList = "";
-		for (Expense expense: expenseService.getExpenses()) {
-			expenseList += "\n\t" + expense + ", date=" + expense.getDate() + ", amount=" + expense.getAmount() + ", currency=" + expense.getCurrency() + ", concept=" + expense.getConcept();
-		}
-		logger.info("Expenses:" + expenseList);
+		logger.info("Hello World");
 	}
 
 }
