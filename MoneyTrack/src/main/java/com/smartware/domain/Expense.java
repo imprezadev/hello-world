662aff5d6ba20883579d69f9850cced83452544d
Impreza Dev <imprezadev@gmail.com>
2017-03-25 20:46:45 -0500

    REFACTOR: let the toString method for Lombok, return even data of super clase

--- MoneyTrack/src/main/java/com/smartware/domain/Expense.java
+++ MoneyTrack/src/main/java/com/smartware/domain/Expense.java
@@ -4,8 +4,10 @@ import com.smartware.domain.catalog.ExpenseCategory;
 import com.smartware.domain.catalog.PaymentType;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(callSuper=true)
 public class Expense extends Transaction {
 	private PaymentType paymenType;
 	private ExpenseCategory category;
