5f6dba4a711e26736dbe33c70dd113520d7012da
impreza Dev <imprezadev@gmail.com>
2017-11-04 20:39:03 -0500

    REFACTOR: no need to check for db engine driver every time getConnection is called, it should be checked just first time.

--- MoneyTrack/src/main/java/com/smartware/common/AppDBHelper.java
+++ MoneyTrack/src/main/java/com/smartware/common/AppDBHelper.java
@@ -1,14 +1,17 @@
 package com.smartware.common;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AppDBHelper {
+
+	private static final Logger logger = Logger.getLogger(AppDBHelper.class.getName());
 	
-	private static final Logger logger = Logger.getLogger(AppDBHelper.class.getName()); 
+	private Driver driver = null;
 
 	private DBConfigParams getDBConfigParams() {
 		DBConfigParams dbConfigParams = null;
@@ -32,14 +35,40 @@ public class AppDBHelper {
 
 		DBConfigParams dbConfigParams = getDBConfigParams();
 		if (dbConfigParams != null) {
-			try {
-				Class.forName(dbConfigParams.getDriver()); // Validating if MySQL JDBC Driver is registered in the classpath
-			} catch (ClassNotFoundException e) {
-				logger.log(Level.SEVERE, "Class not found");
-				e.printStackTrace();
+			if (driver == null) {
+				try {
+					Class jdbcDriverClass = null;
+					try {
+						jdbcDriverClass = Class.forName(dbConfigParams.getDriver()); // Validating if MySQL JDBC Driver is registered in the classpath
+					}
+					catch (ClassNotFoundException e) {
+						logger.log(Level.SEVERE, "Class not found");
+						e.printStackTrace();
+					}
+
+					try {
+						driver = (Driver) jdbcDriverClass.newInstance();
+					}
+					catch (InstantiationException e) {
+						logger.log(Level.SEVERE, e.getMessage());
+						e.printStackTrace();
+					}
+					catch (IllegalAccessException e) {
+						logger.log(Level.SEVERE, e.getMessage());
+						e.printStackTrace();
+					}
+
+					DriverManager.registerDriver(driver);
+				}
+				catch (Exception e) {
+					logger.log(Level.SEVERE, e.getMessage());
+					driver = null;
+					e.printStackTrace();
+				}
 			}
+
 			logger.config("MySQL is registered");
-	
+
 			try {
 				conn = DriverManager.getConnection(dbConfigParams.getUri(), dbConfigParams.getUsername(), dbConfigParams.getPassword());
 			} catch (Exception e) {
