94365ca1625528d43e82f8ffc2b37d13c0271951
impreza Dev <imprezadev@gmail.com>
2017-11-07 12:51:52 -0500

    REFACTOR: Info which is in DBConfigParams class and logic to load it, is just needed by getConnection method so it would be good have them inside AppDBHelper class.

--- MoneyTrack/src/main/java/com/smartware/common/AppDBHelper.java
+++ MoneyTrack/src/main/java/com/smartware/common/AppDBHelper.java
@@ -1,5 +1,6 @@
 package com.smartware.common;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -11,14 +12,34 @@ public class AppDBHelper {
 
 	private static final Logger logger = Logger.getLogger(AppDBHelper.class.getName());
 	
+	private static final String CONFIG_FILENAME = "config.properties";
+
+	DBConfigParams dbConfigParams = null;
 	private Driver driver = null;
+
+	private Properties getFileProperties(String propertiesFileName) {
+		Properties prop = null;
+
+		try {
+			InputStream configPropStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
+
+			if (configPropStream != null) {
+				prop = new Properties();
+				prop.load(configPropStream);
+			} else {
+				logger.log(Level.SEVERE, "property file '" + propertiesFileName + "' not found in the classpath");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return prop;
+	}
 
 	private DBConfigParams getDBConfigParams() {
 		DBConfigParams dbConfigParams = null;
 
-		Utils appHelper = new Utils();
-
-		Properties dbConfigProperties = appHelper.getFileProperties("config.properties");
+		Properties dbConfigProperties = getFileProperties(CONFIG_FILENAME);
 		if (dbConfigProperties != null) {
 			dbConfigParams = new DBConfigParams();
 			dbConfigParams.setDriver(dbConfigProperties.getProperty("driver"));
@@ -27,13 +48,16 @@ public class AppDBHelper {
 			dbConfigParams.setPassword(dbConfigProperties.getProperty("password"));
 		}
 
-		return dbConfigParams; 
+		return dbConfigParams;
 	}
 
 	public Connection getMoneyTrackDBConnection() {
 		Connection conn = null;
 
-		DBConfigParams dbConfigParams = getDBConfigParams();
+		if (dbConfigParams != null) {
+			dbConfigParams = getDBConfigParams();
+		}
+
 		if (dbConfigParams != null) {
 			if (driver == null) {
 				try {
--- MoneyTrack/src/main/java/com/smartware/common/Utils.java
+++ MoneyTrack/src/main/java/com/smartware/common/Utils.java
@@ -1,10 +1,8 @@
 package com.smartware.common;
 
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,25 +14,6 @@ public class Utils {
 	public static final String TIME_DEFAULT_FORMAT = "HH:mm";
 
 	public static final String AMOUNT_FORMAT_PATTERN = "%.2f";
-
-	public Properties getFileProperties(String propertiesFileName) {
-		Properties prop = null;
-		
-		try {
-			InputStream configPropStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
-			
-			if (configPropStream != null) {
-				prop = new Properties();
-				prop.load(configPropStream);
-			} else {
-				logger.log(Level.SEVERE, "property file '" + propertiesFileName + "' not found in the classpath");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return prop;
-	}
 
 	public static String getFormattedDateTime(Date dateTime, String dateTimeFormat) {
 		SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat);
